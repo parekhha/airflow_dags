21973850e71fe555ef45c55559070fa28255f96a
Hardik Dhirendra Parekh <hardik.dhi.parekh@hpe.com>
2019-09-11 06:53:10 +0000

    first commit

---
+++ airflow/contrib/example_dags/example_k8s.py
@@ -0,0 +1,51 @@
+"""
+This is an example dag for using the KubernetesPodOperator.
+"""
+from airflow.utils.dates import days_ago
+from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.models import DAG
+
+log = LoggingMixin().log
+
+try:
+    # Kubernetes is optional, so not available in vanilla Airflow
+    # pip install 'apache-airflow[kubernetes]'
+    from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+
+    args = {
+        'owner': 'Airflow',
+        'start_date': days_ago(2)
+    }
+
+    dag = DAG(
+        dag_id='example_kubernetes_operator',
+        default_args=args,
+        schedule_interval=None)
+
+    tolerations = [
+        {
+            'key': "key",
+            'operator': 'Equal',
+            'value': 'value'
+        }
+    ]
+
+    k = KubernetesPodOperator(
+        namespace='default',
+        image="ubuntu:16.04",
+        cmds=["bash", "-cx"],
+        arguments=["echo", "10"],
+        labels={"foo": "bar"},
+        name="airflow-test-pod",
+        in_cluster=False,
+        task_id="task",
+        get_logs=True,
+        dag=dag,
+        is_delete_operator_pod=False,
+        tolerations=tolerations
+    )
+
+except ImportError as e:
+    log.warning("Could not import KubernetesPodOperator: " + str(e))
+    log.warning("Install kubernetes dependencies with: "
+                "    pip install 'apache-airflow[kubernetes]'")
